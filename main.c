6e5df3b8b60a1e57c1bbd7be51bb7fb36f34b7a5
yueliny <yueliny@a10networks.com>
2019-10-09 16:50:54 +0800

    for test

--- main.c
+++ main.c
@@ -9,34 +9,54 @@
 #include <fcntl.h>
 #include <errno.h>
 
+/* abandoned code 
 typedef unsigned long long      u64;
 typedef unsigned int            u32;
 typedef unsigned short          u16;
 typedef unsigned char           u8;	
+*/
+
+#define	   u64	unsigned long long
+#define    u32	unsigned int
+#define    u16	unsigned short
+#define    u8	unsigned char 
 
 #define HTML_PATH "/var/www/html"
 
 
 /* http request method definition */
+/* abandoned code 
 #define GET		0	
 #define HEAD		1
 #define POST		2
 #define PUT		3
 #define DELET		4
-#define CONNECT		5
+#define CONNECT		5/
 #define OPTIONS		6
 #define TRACE		7
 #define PATCH		8
-
+*/
 
 /* http status code definition */
-#define OK		200
-#define BAD_REQUEST	400
+#define HTTP_STATUS_CODE_OK		200
+#define HTTP_STATUS_CODE_BAD_REQUEST	400
+#define HTTP_STATUS_CODE_NOT_FOUND	404
+
+#define BUFF_SIZE 1500
 
 
-#define buff_size 1500
-#define BAD_REQUEST 400
+enum http_request_method {
 
+	HTTP_METHOD_GET = 0,
+	HTTP_METHOD_HEAD,
+	HTTP_METHOD_POST,
+	HTTP_METHOD_PUT,
+	HTTP_METHOD_DELET,
+	HTTP_METHOD_CONNECT,
+	HTTP_METHOD_OPTIONS,
+	HTTP_METHOD_TRACE,
+	HTTP_METHOD_PATCH
+};
 
 struct http_hdr_info {
 	
@@ -61,19 +81,18 @@ char resp_buff[65535] = {'\0'};
 
 FILE *fin, *fout;
 
-void http_hdr_handler(char *buff) {
+void http_req_hdr_parser(char *buff, struct http_hdr_info *http_req_hdr) {
 
 	printf("%s\n", buff);
 	char *hdr_ptr;
 	hdr_ptr = buff;
 
 	enum http_hdr_state curr_state = start;
-
+	/*
 	struct http_hdr_info *http_hdr = (struct http_hdr_info *)malloc(sizeof(struct http_hdr_info));
 	memset(http_hdr->req_url, '\0', sizeof(http_hdr->req_url));
 	memset(http_hdr->http_version, '\0', sizeof(http_hdr->http_version));
-
-	char http_version[4] = {'\0'};
+	*/
 	int i = 0;
 	char my_url[200] = {'\0'}; 
 	char c;
@@ -84,43 +103,44 @@ void http_hdr_handler(char *buff) {
 		switch (curr_state) {
 
 			case start:
+				/* only implement GET, POST, PUT, CONNECT now*/
 				if ( (*hdr_ptr == 'C') && (*(hdr_ptr + 1) == 'N') && (*(hdr_ptr + 2) == 'N') && (*(hdr_ptr + 3) == 'E')
 					 	   && (*(hdr_ptr + 4) == 'C') && (*(hdr_ptr + 5) == 'T') ) {
 					printf("is CONNECT\n");
-					http_hdr->req_method = CONNECT;
+					http_req_hdr->req_method = HTTP_METHOD_CONNECT;
 					hdr_ptr += 5;
 				}
 				else if ( (*hdr_ptr == 'G') && (*(hdr_ptr + 1) == 'E') && (*(hdr_ptr + 2) == 'T') ) {
 					printf("is GET \n");
-					http_hdr->req_method = GET;
+					http_req_hdr->req_method = HTTP_METHOD_GET;
 					hdr_ptr += 2; 
 				}
 				else if ( *hdr_ptr == 'P' ) {
 					if( (*(hdr_ptr + 1) == 'O') && (*(hdr_ptr + 2) == 'S') && (*(hdr_ptr + 3) == 'T') ) {
 						printf("is POST \n");
-						http_hdr->req_method = POST;
+						http_req_hdr->req_method = HTTP_METHOD_POST;
 						hdr_ptr += 3; 
 					}
 					else if( (*(hdr_ptr + 1) == 'U') && (*(hdr_ptr + 2) == 'T') ) {
 						printf("is PUT \n");
-						http_hdr->req_method = PUT;
+						http_req_hdr->req_method = HTTP_METHOD_PUT;
 						hdr_ptr += 2;
 					}
 					else {
-						http_hdr->http_status_code = BAD_REQUEST;
+						http_req_hdr->http_status_code = HTTP_STATUS_CODE_BAD_REQUEST;
 						printf("is bad request \n");
 						return;
 					}
 				}
 				else {
-					http_hdr->http_status_code = BAD_REQUEST;
+					http_req_hdr->http_status_code = HTTP_STATUS_CODE_BAD_REQUEST;
 					printf("is bad request \n");
 					return;
 				}
 				
 				if ( *(hdr_ptr + 1) != ' ') {
-					http_hdr->http_status_code = BAD_REQUEST;
-					printf("bad request \n");
+					http_req_hdr->http_status_code = HTTP_STATUS_CODE_BAD_REQUEST;
+					printf("is bad request \n");
 					return;
 				}
 				else {
@@ -134,16 +154,16 @@ void http_hdr_handler(char *buff) {
 				/* url handle */
 				while ( *(hdr_ptr + i) != ' ' ) {
 					//url_test[i] = *(hdr_ptr + i);
-					http_hdr->req_url[i] = *(hdr_ptr + i);
+					http_req_hdr->req_url[i] = *(hdr_ptr + i);
 					i++;
 				}
-				printf("url: %s \n", http_hdr->req_url);
+				printf("url: %s \n", http_req_hdr->req_url);
 				hdr_ptr += i;
 				hdr_ptr++;
 				
 				/* read the file of url */
 				strcpy(my_url, HTML_PATH);
-				strncat(my_url, http_hdr->req_url, (i + 1));
+				strncat(my_url, http_req_hdr->req_url, (i + 1));
 				printf("PWD: %s \n", my_url);
 				fin = fopen(my_url, "r");
 				if(fin == NULL) {
@@ -165,16 +185,16 @@ void http_hdr_handler(char *buff) {
 					&& (*(hdr_ptr + 4) == '/') ) {
 					hdr_ptr += 5;
 					i = 0;
-					while ( *(hdr_ptr + i) != 0x0d ) {   // 0x0d = '/r'
+					while ( *(hdr_ptr + i) != '\r' ) {   // 0x0d = '\r'
 	
-						http_version[i] = *(hdr_ptr + i);
+						http_req_hdr->http_version[i] = *(hdr_ptr + i);
 						i++;	
 					}
-					printf("version: %s \n", http_version);
+					printf("version: %s \n", http_req_hdr->http_version);
 					hdr_ptr += (i - 1); 
 				}
 				else {
-					http_hdr->http_status_code = BAD_REQUEST;
+					http_req_hdr->http_status_code = HTTP_STATUS_CODE_BAD_REQUEST;
 					printf("bad request \n");
 				}
 
@@ -188,10 +208,10 @@ void http_hdr_handler(char *buff) {
 				break;
 
 			case msg_body:
-				if (http_hdr->req_method == PUT) {
+				if (http_req_hdr->req_method == HTTP_METHOD_PUT) {
 					// PUT func
 				}
-				else if (http_hdr->req_method == POST) {
+				else if (http_req_hdr->req_method == HTTP_METHOD_POST) {
 					// POST func
 				}
 				break;
@@ -200,7 +220,7 @@ void http_hdr_handler(char *buff) {
 				if ( *hdr_ptr == '\r' )
 					curr_state = LF;
 				else {
-					http_hdr->http_status_code = BAD_REQUEST;
+					http_req_hdr->http_status_code = HTTP_STATUS_CODE_BAD_REQUEST;
 					printf("bad request \n");
 					return;
 				}
@@ -217,7 +237,7 @@ void http_hdr_handler(char *buff) {
 						curr_state = msg_hdr;
 				}
 				else {
-					http_hdr->http_status_code = BAD_REQUEST;
+					http_req_hdr->http_status_code = HTTP_STATUS_CODE_BAD_REQUEST;
 					printf("bad request \n");
 					return;
 				}
@@ -235,16 +255,52 @@ void http_hdr_handler(char *buff) {
 }
 
 
-void http_resp_hdr_fill() {
+/* get URI file for GET */
+void file_get() {
 
-	char resp_hdr[2048] = {'\0'};
+}
+
+
+void http_req_process(char *buff, struct http_hdr_info *http_req_hdr) {
+
+	http_req_hdr_parser(buff, http_req_hdr);
+
+	if ( http_req_hdr->http_method == HTTP_METHOD_GET ) {
+		
+		FILE *file_for_GET;
+		if ( (file_for_GET = fopen(, "r")) ) {
+
+		}
+		else {
+			http_req_hdr->http_status_code = HTTP_STATUS_CODE_NOT_FOUND;
+		}
+	}
+	else {
+		// not implement yet
+	}
+	
+
+}
+
+
+void http_resp_hdr_fill(char *resp_hdr, struct http_hdr_info *http_req_hdr) {
+
+	//char resp_hdr[2048] = {'\0'};
 	int buff_end;
 
-	/* start line */
+	/* start line */	
+	char *http_str = "HTTP/";
+	char *my_http_version = "1.0 ";
+	strcpy(resp_hdr, http_str);
+	strcat(resp_hdr, my_http_version);
+	//strcat(resp_hdr, http_req_hdr->http_version);
+
 	
+	
+	printf("<<<%s>>>\n", resp_hdr);
 	/* for http */
-	resp_hdr[0] = 'H'; resp_hdr[1] = 'T'; resp_hdr[2] = 'T'; resp_hdr[3] = 'P'; resp_hdr[4] = '/';
-	buff_end = 5;
+	//resp_hdr[0] = 'H'; resp_hdr[1] = 'T'; resp_hdr[2] = 'T'; resp_hdr[3] = 'P'; resp_hdr[4] = '/';
+	//buff_end = 5;
 	/*
 	int x = 0;
 	while( buff_end != 8 ) { // for example: [5]='1', [6]='.', [7]='1'
@@ -260,9 +316,9 @@ void http_resp_hdr_fill() {
 	/* msg header */
 
 	/* CRLF */
-	resp_hdr[buff_end] = '\r';
-	resp_hdr[buff_end] = '\n';
-	buff_end += 2;
+	//resp_hdr[buff_end] = '\r';
+	//resp_hdr[buff_end] = '\n';
+	//buff_end += 2;
 
 	/* msg body */
 
@@ -301,31 +357,45 @@ int main(int argc, char **argv) {
 		printf("listening ... \n");
 	}
 
-	cli_addr_len = sizeof(cli_addr);
-	if ( (conn_fd = accept(listen_fd, (struct sockaddr *)(&cli_addr), &cli_addr_len)) < 0 ) {
-		printf("accept() failed \n");
-		exit(0);
+	while (1) {
+		cli_addr_len = sizeof(cli_addr);
+		if ( (conn_fd = accept(listen_fd, (struct sockaddr *)(&cli_addr), &cli_addr_len)) < 0 ) {
+			printf("accept() failed \n");
+			exit(0);
+		}
+
+		int res = 0;
+		char buff[BUFF_SIZE] = {0};
+		/*
+		 *  If no messages are available at the socket, the receive calls wait
+		 *  for a message to arrive, unless the socket is nonblocking (see fcntl(2))
+		 */
+		res = recv(conn_fd, buff , sizeof(buff), 0);
+		if ( res <= 0 ) {
+			printf("recv() failed \n");
+			exit(0);
+		}
+
+		struct http_hdr_info *http_req_hdr = (struct http_hdr_info *)malloc(sizeof(struct http_hdr_info));
+        	memset(http_req_hdr->req_url, '\0', sizeof(http_req_hdr->req_url));
+        	memset(http_req_hdr->http_version, '\0', sizeof(http_req_hdr->http_version));
+
+		http_req_process(buff, http_req_hdr);
+
+		char resp_hdr[2048] = {'\0'};
+		http_resp_hdr_fill(resp_hdr, http_req_hdr);
+
+		/* temporary use */
+		char *test = "HTTP/1.0 200 OK\r\nServer: william_server\r\nAccept-Ranges: bytes\r\nContent-Length: 265\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n";
+		char temp[200] = {'\0'};
+		strcpy(temp, test);
+		strncat(temp, resp_buff, strlen(resp_buff));
+		printf("temp string: %s \n strlen: %ld \n", temp, strlen(temp));
+		send(conn_fd, temp, strlen(temp), 0);
+		
+		close(conn_fd); 
+		printf("socket closed \n");
 	}
-
-	int res = 0;
-	char buff[buff_size] = {0};
-	/*
-	 *  If no messages are available at the socket, the receive calls wait
-	 *  for a message to arrive, unless the socket is nonblocking (see fcntl(2))
-	 */
-	res = recv(conn_fd, buff , sizeof(buff), 0);
-	http_hdr_handler(buff);
-
-	/* temporary use */
-	char *test = "HTTP/1.0 200 OK\r\nServer: william_server\r\nAccept-Ranges: bytes\r\nContent-Length: 265\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n";
-	char temp[200] = {'\0'};
-	strcpy(temp, test);
-	strncat(temp, resp_buff, strlen(resp_buff));
-	printf("temp string: %s \n strlen: %d \n", temp, strlen(temp));
-	send(conn_fd, temp, strlen(temp), 0);
-	
-	close(conn_fd); 
-	printf("socket closed \n");
 
 	return 0;
         
